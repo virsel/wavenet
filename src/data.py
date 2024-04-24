09c989a7af65e9b1a5afe765466c578b2afb511d
virsel <paul-hornig@gmx.de>
2024-04-24 20:10:27 +0200

    Add .gitignore file and implement get_train_test_split function in data.py

---
+++ src/data.py
@@ -0,0 +1,16 @@
+import torch
+from sklearn.model_selection import train_test_split
+
+def get_train_test_split(x_series, y_series, context_length = 30, test_size=0.2):
+    vocab = set(x_series.str.cat(sep=' ').split())
+    vocab = sorted(list(vocab))
+    # print vocab size
+    stoi = {s:i+1 for i,s in enumerate(vocab)}
+    stoi['.'] = 0
+    print(f'Vocab size: {len(stoi)}')
+    X = x_series.apply(lambda x: [stoi[v] for v in x.split()])
+    X = X.apply(lambda x: [0]*(context_length-len(x)) + list(x) if len(x) < context_length  else x[:context_length])
+    X = torch.tensor(X)
+    torch.nn.functional.one_hot(X[0], num_classes=len(stoi)).float()
+    Y = torch.tensor(y_series)
+    return train_test_split(X, Y, test_size=0.2, stratify=Y)
